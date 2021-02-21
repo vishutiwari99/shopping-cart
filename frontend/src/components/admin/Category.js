a86f1144199141c557e25a2599a09f16cdd63ade
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-02-21 16:17:30 +0530

    Category responsiveness bug fixed

--- frontend/src/components/admin/Category.js
+++ frontend/src/components/admin/Category.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import './Category.css'
+import Loading from '../Loading'
 
-import Loading from '../../components/Loading'
 
-import { createCategory, getAllCategories } from '../../redux/actions/categoryActions'
+import { getAllCategories, deleteCategory } from '../../redux/actions/categoryActions'
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -14,13 +15,20 @@ function Category() {
     const [name, setName] = useState("");
     const [onEdit, setOnEdit] = useState(false);
     const [id, setId] = useState("");
-    const [callback, setCallback] = useState(false)
+    const [callback, setCallback] = useState(false);
+    const [error, setError] = useState(false);
 
 
     useEffect(() => {
         dispatch(getAllCategories());
-        console.log("yaha")
     }, [dispatch, callback])
+
+    const editCategory = async (id, name) => {
+        setId(id)
+        setName(name)
+        setOnEdit(true)
+    }
+
 
     const createCategoryHandler = async (e) => {
         e.preventDefault();
@@ -31,58 +39,66 @@ function Category() {
                 },
             };
             if (onEdit) {
-                const res = await axios.put(`/api/category/${id}`, { name }, config)
+                const res = await axios.put(`/api/category/${id}`, { name }, config);
+                setCallback(!callback);
+                setName("");
                 alert(res.data.msg)
             } else {
                 const res = await axios.post(`/api/category`, { name }, config)
-                setCallback(true);
+                setCallback(!callback);
                 setName("");
                 alert(res.data.msg)
             }
             setOnEdit(false)
             setName('');
 
-        } catch (error) {
-            console.log(error);
+        } catch (er) {
+            setError(true);
+            setName("");
+            console.log(er);
         }
     }
 
-    const deleteCategoryHandler = (id) => {
-
+    const deleteCategoryHandler = async (id) => {
+        try {
+            const res = await axios.delete(`/api/category/${id}`);
+            // alert(res.data.msg);
+            setCallback(!callback);
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
     }
+
+
 
     if (!categories) return <div className="loading"><Loading /></div>
 
     return (
-        <div className="categories">
-            <form onSubmit={createCategoryHandler}>
-                <label htmlFor="category">Category</label>
-                <input type="text" name="category" value={name} onChange={(e) => setName(e.target.value)} required
-                />
 
-                <button type="submit">Create</button>
+        <div className='category_conatiner'>
+            <h2>{error ? `Category already exist` : "Categories"} </h2>
+            <form className="category_form" onSubmit={createCategoryHandler}>
+                <input type="text" className="category-form-control" id="addProductTextBox" placeholder="Enter Category Name" value={name} onChange={(e) => setName(e.target.value)} required />
+                <button className="category-btn category-btn-primary" id="addProductBtn" type='submit'>{onEdit ? "UPDATE" : "ADD CATEGORY"}</button>
             </form>
-
-            <div className="col">
-                {
-                    categories.map(category => (
-                        <div className="row" key={category._id}>
-                            <p>{category.name}</p>
-                            <div>
-                                <button >Edit</button>
-                                <button onClick={deleteCategoryHandler(category._id)}>Delete</button>
-                            </div>
+            <ul id="productList">{
+                categories.map(category => (
+                    <li className="category_row" key={category._id}>
+                        <span className="text">{category.name}</span>
+                        <div className="category-btn-conatiner">
+                            <button className="category-btn category-btn-primary" onClick={() => editCategory(category._id, category.name)}>Edit</button>
+                            <button className="category-btn category-delete" onClick={() => deleteCategoryHandler(category._id)}> delete</button>
                         </div>
+                    </li>
+                ))
+            }
 
-                    ))
-                }
+            </ul>
 
-            </div>
 
         </div>
 
     )
-
 }
 
 export default Category
